b6553d7cc8996f7360bbe97cfd503e3d16d563c5
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2025-04-17 15:23:16 +0200

    chore: init project

---
+++ App/Models/Calculator.cs
@@ -0,0 +1,6 @@
+namespace ModernSoftware.App.Models;
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+}
---
+++ Test/CalculatorTests.cs
@@ -0,0 +1,13 @@
+﻿using ModernSoftware.App.Models;
+
+namespace ModernSoftware.Test;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ShouldReturnCorrectSum()
+    {
+        var calc = new Calculator();
+        Assert.Equal(5, calc.Add(2, 3));
+    }
+}
